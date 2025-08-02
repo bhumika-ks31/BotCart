c9e373a1b980b20b93549c2a54c6128159a6ff43
Bhumika kashyap <tilantajagra@gmail.com>
2025-08-02 19:59:37 +0530

    Update AuthContext.jsx

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext } from 'react'
 export const authDataContext= createContext()
 function AuthContext({children}) {
-    let serverUrl = "https://botcart.onrender.com"
+    let serverUrl = "https://botcart-31.onrender.com"
 
     let value = {
        serverUrl
